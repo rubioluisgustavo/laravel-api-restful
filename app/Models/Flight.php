62ad122386adf5f280de9e3c70f4a3d371892248
Luis Rubio <rubioluisgustavo@gmail.com>
2024-12-11 23:21:08 -0300

    new table

---
+++ app/Models/Flight.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class Flight extends Model
+{
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+}
